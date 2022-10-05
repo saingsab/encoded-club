0358bdf1fa3f2f884c24bd5a3e8e26ce55d33588
Saing <saing.sab@gmail.com>
2022-10-06 01:00:31 +0700

    add l10

---
+++ l10/contracts/TokenSale.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IMyERC20Token {
+     function mint(address to, uint256 amount) external;
+}
+
+contract TokenSale {
+    /// @notice Purchase Ratio between Sale ERC20 and Ether
+    uint256 public ratio;
+    IMyERC20Token public  paymentToken;
+
+    constructor(uint256 _ratio, address _paymentToken) {
+        ratio = _ratio;
+        paymentToken = IMyERC20Token(_paymentToken);
+    }
+
+    function purchaseTokens() public payable {
+        uint256 etherReceived = msg.value;
+        uint256 tokenTobeEarned = etherReceived / ratio;
+        paymentToken.mint(msg.sender, tokenTobeEarned);
+    }
+
+}
